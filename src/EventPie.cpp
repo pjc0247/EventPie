f7d7d67abdc4ef931fbb0dbf59ab396070596090
Park June Chul <pjc0247@naver.com>
2014-04-27 20:52:56 +0900

    Create EventPie.cpp

---
+++ src/EventPie.cpp
@@ -0,0 +1,52 @@
+#include "EventPie.h"
+
+#include "Event.h"
+#include "Timer.h"
+
+#include <queue>
+#include <vector>
+#include <chrono>
+
+using namespace std;
+using namespace std::chrono;
+
+namespace EventPie{
+    
+    static vector<Timer*> timers;
+    
+    static bool running;
+    
+    int run(){
+        
+        running = true;
+        
+        auto timePoint = system_clock::now();
+        
+        while( running ){
+            
+            auto loopTime =
+                duration_cast<milliseconds>(system_clock::now() - timePoint).count();
+            
+            if( loopTime ){
+                timePoint = system_clock::now();
+            
+                for( auto &timer : timers ){
+                    timer->step( loopTime );
+                }
+            }
+        }
+        return 0;
+    }
+    
+    void addTimer(Timer *timer){
+        timers.push_back( timer );
+    }
+    void removeTimer(Timer *timer){
+        for(int i=0;i<timers.size();i++){
+            if( timers[i] == timer ){
+                std::swap( timers[i], timers.back() );
+                timers.pop_back();
+            }
+        }
+    }
+}

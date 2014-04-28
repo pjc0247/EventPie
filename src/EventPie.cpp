8a9b949f18e0f867754768dd4745b7dea0633f8b
Park June Chul <pjc0247@naver.com>
2014-04-28 13:06:20 +0900

    Update EventPie.cpp

--- src/EventPie.cpp
+++ src/EventPie.cpp
@@ -1,12 +1,13 @@
 #include "EventPie.h"
 
-#include "Event.h"
+#include "Reaction.h"
 #include "Timer.h"
-#include "Task.h"
+#include "ThreadPool.h"
 
 #include <queue>
 #include <vector>
 #include <chrono>
+#include <mutex>
 
 using namespace std;
 using namespace std::chrono;
@@ -14,8 +15,31 @@ using namespace std::chrono;
 namespace EventPie{
     
     static vector<Timer*> timers;
-
+    static queue<Task> tasks;
+    
+    static mutex taskQueueMutex;
+    
+    static ThreadPool threadPool;
+    
     static bool running;
+    
+    void processTasks(){
+        /* process task queue */
+        unique_lock<mutex> lock( taskQueueMutex );
+        while( !tasks.empty() ){
+            Task task = tasks.front();
+                task();
+            tasks.pop();
+        }
+        lock.unlock();
+
+    }
+    void processTimers(int dt){
+        /* process timers */
+        for( auto &timer : timers ){
+            timer->step( dt );
+        }
+    }
     
     int run(){
         running = true;
@@ -23,20 +47,17 @@ namespace EventPie{
         auto timePoint = system_clock::now();
         
         while( running ){
-            /* process event queue */
-            
+            processTasks();
             if( !running ) break;
             
-            /* process timers */
+            /* calculate loop time */
             auto loopTime =
                 duration_cast<milliseconds>(system_clock::now() - timePoint).count();
             
             if( loopTime ){
                 timePoint = system_clock::now();
             
-                for( auto &timer : timers ){
-                    timer->step( (int)loopTime );
-                }
+                processTimers( (int)loopTime );
             }
         }
         return 0;
@@ -49,11 +70,21 @@ namespace EventPie{
         timers.push_back( timer );
     }
     void removeTimer(Timer *timer){
+        /* fast-remove */
         for(int i=0;i<timers.size();i++){
             if( timers[i] == timer ){
                 std::swap( timers[i], timers.back() );
                 timers.pop_back();
             }
         }
+    }
+    
+    void addTask(Task task){
+        unique_lock<mutex> lock( taskQueueMutex );
+            tasks.push( task );
+    }
+    
+    void defer(Task task){
+        threadPool.enqueue( task );
     }
 }

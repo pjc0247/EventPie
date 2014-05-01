bc04af0fb6f6ff9eec76153dffc771ba41f2cd2f
Park June Chul <pjc0247@naver.com>
2014-05-01 14:00:27 +0900

    Update Util.h

--- src/Util.h
+++ src/Util.h
@@ -4,20 +4,20 @@
 #define EP_SAFE_CALL_0(f) do{\
     if( f != nullptr )\
         f();\
-    }while(0);
+    }while(0)
 #define EP_SAFE_CALL(f,...) do{\
     if( f != nullptr )\
         f( __VA_ARGS__ );\
-    }while(0);
+    }while(0)
 
 #define EP_SAFE_DEFER_0(f) do{\
     if( f != nullptr ){\
         EventPie::defer( f );\
-    }}while(0);
+    }}while(0)
 #define EP_SAFE_DEFER(f,...) do{\
     if( f != nullptr ){\
         EventPie::defer(\
             std::bind(f,__VA_ARGS__) );\
-    }}while(0);
+    }}while(0)
 
 #endif //_UTIL_H

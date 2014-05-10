67364e773e5a412fead650be7b1d0fd5ed4d29f8
Park June Chul <pjc0247@naver.com>
2014-05-10 21:09:25 +0900

    Update Config.h

--- src/Config.h
+++ src/Config.h
@@ -2,8 +2,12 @@
 #define _CONFIG_H
 
 namespace EventPie{
-    const int cfgBacklogSize = 1024;
-    const int cfgThreadPoolSize = 20;
+    namespace Config{
+        const int backlogSize = 1024;
+        const int threadPoolSize = 20;
+        
+        const int evMaxEvents = 1024;
+    };
 };
 
 #endif //_CONFIG_H

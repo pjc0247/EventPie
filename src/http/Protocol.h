acdc490210189dc7d133a0c44e13eb6a83c5ae3d
Park June Chul <pjc0247@naver.com>
2014-05-10 21:04:49 +0900

    Create Protocol.h

---
+++ src/http/Protocol.h
@@ -0,0 +1,14 @@
+#ifndef _HTTP_PROTOCOL_H
+#define _HTTP_PROTOCOL_H
+
+namespace EventPie{
+    namespace Http{
+    
+        enum Method{
+            eGet = 0,
+            ePost
+        };
+    };
+};
+
+#endif //_HTTP_PROTOCOL_H

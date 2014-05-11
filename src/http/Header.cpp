f12db648ee11f0c22ff1117fbe6cefc85b3bcca4
Park June Chul <pjc0247@naver.com>
2014-05-11 23:08:54 +0900

    Update Header.cpp

--- src/http/Header.cpp
+++ src/http/Header.cpp
@@ -1,5 +1,7 @@
 #include "Header.h"
 
+#include "../ParseUtil.h"
+
 using namespace std;
 
 namespace EventPie{
@@ -26,7 +28,40 @@ namespace EventPie{
         }
         
         void Header::load(const string &header){
-        
+            int offset = 0;
+            for(int i=0;i<header.size()-1;i++){
+                
+                /* check CrLf */
+                if( EP_STRCMP2( &header[i], '\r','\n' ) ){
+                    
+                    /* parse field */
+                    if( i != offset ){ /* for ignore empty field */
+                        int ws1 = 0, ws2 = 0;
+                        int delim = -1;
+                        
+                        for(int j=offset;j<i;j++){
+                            if( header[j] == ' ' ){
+                                if( delim == -1 ) ws1 ++;
+                                else              ws2 ++;
+                            }
+                            else if( delim > 0 ){
+                                setField(
+                                    header.substr( offset, delim-offset-ws1 ),
+                                    header.substr( offset+ws2+1, i-(delim+ws2+1) ) );
+                                break;
+                            }
+                            else if( header[j] == ':' )
+                                delim = j;
+                        }
+                    }
+                    offset = i+2;
+                }
+                /* check double-CrLf */
+                if( i+3 < header.size() &&
+                    EP_STRCMP4( &header[i], '\r','\n','\r','\n' ) ){
+                    break;
+                }
+            }
         }
         string Header::dump() const{
             string header;

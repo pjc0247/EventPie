5597f284cbc6d1c0650fb4852125ddd187522b8a
Park June Chul <pjc0247@naver.com>
2014-05-14 22:52:51 +0900

    Update echo_server.cpp

--- example/echo_server.cpp
+++ example/echo_server.cpp
@@ -7,23 +7,23 @@ using namespace EventPie::Io;
 const int port = 9916;
 
 int main(int argc,char **argv){
-  TCPServer *server;
+    TCPServer *server;
   
-  server = new TCPServer( port,
-    [server](TCPClient *client){
-      printf("new connection\n");
+    server = new TCPServer( port,
+        [server](TCPClient *client){
+            printf("new connection\n");
       
-      client->onReceive(
-        [client](void *data, int len){
-          client->write( data, len );
-        });
-      client->onUnbind(
-        [client](){
-          printf("client disconnected\n");
-        });
+            client->onReceive(
+                [client](void *data, int len){
+                    client->write( data, len );
+            });
+            client->onUnbind(
+                [client](){
+                    printf("client disconnected\n");
+            });
     });
     
-  run();
-  
-  return 0;
+    run();
+
+    return 0;
 }

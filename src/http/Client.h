ed89c5dfb408018e54f877309edbc574e770916e
Park June Chul <pjc0247@naver.com>
2014-05-21 12:33:04 +0900

    Update Client.h

--- src/http/Client.h
+++ src/http/Client.h
@@ -4,25 +4,51 @@
 #include <functional>
 #include <string>
 
+#include "../io/TCPSocket.h"
+
+#include "Protocol.h"
 #include "Uri.h"
 
 namespace EventPie{
     namespace Http{
-        typedef std::function<void(int, std::string,std::string)> ResponseCallback;
+        
+        class Header;
+        typedef std::function<void(int, Header,std::string)> ResponseCallback;
         
         class Client{
         public:
             Client();
-            Client(const char *uri, ResponseCallback callback);
-            Client(Uri &uri, ResponseCallback callback);
+            Client(
+                const std::string &uri,
+                ResponseCallback callback);
+            Client(
+                Uri uri,
+                ResponseCallback callback);
             virtual ~Client();
             
         protected:
-            void open();
-            void openAsync();
+            void open(const std::string &host, int port);
+            
+            void request(
+                Method method, Uri uri,
+                void *data=nullptr, int len=0);
+            void request(
+                Method method, Uri uri,
+                Header header,
+                void *data=nullptr, int len=0);
+            
+            void requestAsync(
+                Method method, Uri uri,
+                void *data=nullptr, int len=0);
+            void requestAsync(
+                Method method, Uri uri,
+                Header header,
+                void *data=nullptr, int len=0);
 
         protected:
             Uri uri;
+            
+            Io::TCPSocket *socket;
             
             ResponseCallback responseCallback;
         };
